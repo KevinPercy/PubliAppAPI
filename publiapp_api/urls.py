6527ce23e986e1f2464364ef4f667638af566541
KevinPercy <kevinpercycl@gmail.com>
2020-07-08 23:04:32 -0500

    se agregan las url de los modelos

--- publiapp_api/urls.py
+++ publiapp_api/urls.py
@@ -11,8 +11,12 @@ router.register('negocios', views.NegocioViewSet)
 router.register('servicios', views.ServicioViewSet)
 router.register('categoria', views.CategoriaViewSet)
 router.register('resenia', views.ReseniaViewSet)
+router.register('rol', views.RolViewSet)
+router.register('anunciante', views.AnuncianteViewSet)
+# router.register('imagen', views.ImagenUploadView)
 
 urlpatterns = [
+    path('imagen/', views.ImagenUploadView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
